1ff8acba53304997e44dc47b189736a40f318325
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2023-05-30 17:20:20 +0300

    Added the student page and firebase

---
+++ firebase/index.ts
@@ -0,0 +1,120 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GithubAuthProvider } from "firebase/auth";
+import { getFirestore, collection, getDocs, addDoc ,onSnapshot} from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// ...
+
+// Initialize Firebase
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNTDxwjpCnQmULUxeHoIhzQYa6Cwhzlvs",
+  authDomain: "codust-tutorial.firebaseapp.com",
+  projectId: "codust-tutorial",
+  storageBucket: "codust-tutorial.appspot.com",
+  messagingSenderId: "989795830765",
+  appId: "1:989795830765:web:e1fccbcc9d6fd28e6b75e4",
+  measurementId: "G-FX9FGYJVLC",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Get the Auth instance
+const auth = getAuth(app);
+const db = getFirestore();
+
+// Collections
+const colRef = collection(db, "assignments");
+
+// Handlers
+const gitHubProvider = new GithubAuthProvider();
+
+// signInStudentHandler
+/**
+ * This will sign In the students
+ * @returns void
+ */
+const signInStudentHandler = async (): Promise<ReloadUserInfo> => {
+  try {
+    const result = await signInWithPopup(auth, gitHubProvider);
+    const user: StudentType = result.user as StudentType;
+    if (user) {
+      const reloadUserInfo = user.reloadUserInfo as ReloadUserInfo;
+      localStorage.setItem("student", JSON.stringify(reloadUserInfo));
+      return reloadUserInfo;
+    }
+  } catch (error) {
+    console.error("Sign-in error:", error);
+  }
+  return null;
+};
+
+
+// Get collections data
+const getAllStudents = async (uid: string) => {
+  return new Promise<any[]>((resolve, reject) => {
+    const assignments: any[] = [];
+
+    const unsubscribe = onSnapshot(colRef, (snapshot) => {
+      assignments.length = 0; // Clear the assignments array
+
+      snapshot.docs
+        .filter((doc) => doc.data().uid === uid)
+        .forEach((doc) => assignments.push(doc.data()));
+
+      console.log(assignments);
+
+      resolve(assignments); // Resolve the promise with the assignments array
+    });
+
+    // Unsubscribe from the snapshot listener when needed
+    // For example, if you want to stop listening after a certain condition is met
+    // unsubscribe();
+  });
+};
+const addAssignment = async (assignment: Assignment, uid: string) => {
+  try {
+    const assignmentRef = collection(db, "assignments");
+    const newAssignment = { ...assignment, uid };
+    await addDoc(assignmentRef, newAssignment);
+    console.log("Assignment added successfully");
+  } catch (error) {
+    console.error("Error adding assignment:", error);
+  }
+};
+
+// Types
+type StudentType = {
+  reloadUserInfo?: ReloadUserInfo;
+};
+
+type ReloadUserInfo = {
+  createdAt: string;
+  displayName: string;
+  localId: string;
+  photoUrl: string;
+  providerUserInfo:{
+    email:string
+  }
+};
+
+type Assignment = {
+  part: string;
+  usedHours: number;
+  repository: string;
+  comment: string;
+};
+
+// Exports
+export default app;
+export {
+  app,
+  signInStudentHandler,
+  getAllStudents,
+  StudentType,
+  ReloadUserInfo,
+  Assignment,
+  addAssignment,
+};
---
+++ src/pages/courses/student/index.tsx
@@ -0,0 +1,305 @@
+import {
+  Assignment,
+  ReloadUserInfo,
+  StudentType,
+  addAssignment,
+  getAllStudents,
+  signInStudentHandler,
+} from "@site/firebase";
+import React, { useState, useEffect } from "react";
+
+const AssignmentPage = () => {
+  const [student, setStudent] = useState<ReloadUserInfo>(null);
+  const [allAssignments, setAllAssignments] = useState([]);
+  const [displayAssignmentForm, setDisplayAssignmentForm] = useState(false);
+  const [submited, setSubmited] = useState(false);
+  const handlersetSubmited = () => {
+    setSubmited((p) => !p);
+  };
+  const displayAssignmentFormHandler = () => {
+    setDisplayAssignmentForm((p) => !p);
+    setSubmited((p) => !p);
+  };
+
+  useEffect(() => {
+    const storedStudent = localStorage.getItem("student");
+    if (storedStudent) {
+      const parsedStudent = JSON.parse(storedStudent);
+      setStudent(parsedStudent);
+    }
+  }, []);
+
+  useEffect(() => {
+    const gaa = async () => {
+      if (student) setAllAssignments(await getAllStudents(student.localId));
+    };
+    gaa();
+
+    return () => {
+      null;
+    };
+  }, [student, submited]);
+
+  return (
+    <>
+      <div
+        style={{
+          display: "grid",
+          placeItems: "center",
+          padding: 10,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            // textAlign: "center",
+            width: "90%",
+          }}
+        >
+          <h1>Assignment Page</h1>
+          <div>
+            {student ? (
+              <div
+                style={{
+                  display: "flex",
+                  alignContent: "center",
+                }}
+              >
+                <img
+                  src={student.photoUrl}
+                  alt={student.displayName}
+                  style={{
+                    height: "50px",
+                    width: "50px",
+                    borderRadius: "50%",
+                  }}
+                />
+                <div>
+                  <h5>{student.displayName}</h5>
+                  <p>{student.providerUserInfo[0].email}</p>
+                </div>
+              </div>
+            ) : (
+              <>
+                <button
+                  onClick={async () => {
+                    setStudent(await signInStudentHandler());
+                  }}
+                >
+                  SignIn/SignUp
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+        {student && (
+          <>
+            <h1>Hello There {student.displayName} !</h1>
+            {displayAssignmentForm ? (
+              <AssignmentForm
+                displayAssignmentFormHandler={displayAssignmentFormHandler}
+                uid={student.localId}
+              />
+            ) : (
+              <Button
+                displayAssignmentFormHandler={displayAssignmentFormHandler}
+              >
+                Create Terminal Exercise
+              </Button>
+            )}
+            {allAssignments.length > 0 && (
+              <Table allAssignments={allAssignments} />
+            )}
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default AssignmentPage;
+const Button = ({ children, displayAssignmentFormHandler }) => {
+  const [buttonStyle, setButtonStyle] = useState({
+    backgroundColor: "#4caf50",
+    color: "white",
+    padding: "10px 20px",
+    border: "none",
+    borderRadius: "4px",
+    cursor: "pointer",
+    margin: "15px",
+  });
+
+  const hoverStyle = {
+    backgroundColor: "#45a049",
+  };
+
+  const handleMouseOver = () => {
+    setButtonStyle((prevStyle) => {
+      return { ...prevStyle, ...hoverStyle };
+    });
+  };
+
+  return (
+    <button
+      style={buttonStyle}
+      onClick={displayAssignmentFormHandler}
+      onMouseOver={handleMouseOver}
+    >
+      {children}
+    </button>
+  );
+};
+
+const Table = ({ allAssignments }: { allAssignments: Assignment[] }) => {
+  const totalHours = allAssignments.reduce(
+    (sum, assignment) => sum + assignment.usedHours,
+    0
+  );
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Part</th>
+          <th>Hours</th>
+          <th>GitHub</th>
+          <th>Comment</th>
+          <th>Example Solutions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {allAssignments.map((item, index) => (
+          <tr key={index}>
+            <td>{item.part}</td>
+            <td>{item.usedHours}</td>
+            <td>
+              <a
+                href={item.repository}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.repository}
+              </a>
+            </td>
+            <td>{item.comment}</td>
+          </tr>
+        ))}
+        <tr>
+          <td colSpan={1}>Total</td>
+          <td>{totalHours}</td>
+          <td colSpan={3}></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const AssignmentForm: React.FC<{
+  displayAssignmentFormHandler: () => void;
+  uid: string;
+}> = ({ displayAssignmentFormHandler, uid }) => {
+  const [repository, setRepository] = useState("");
+  const [comment, setComment] = useState("");
+
+  const [hours, setHours] = useState<number>(0); // Set an initial value for hours
+
+  const handleHoursChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const parsedValue = Number(value);
+
+    if (!isNaN(parsedValue)) {
+      setHours(parsedValue);
+    } else {
+      setHours(0);
+    }
+  };
+
+  const handleRepositoryChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRepository(event.target.value);
+  };
+
+  const handleCommentChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setComment(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // Perform submission logic here
+    // You can access the form values using the 'hours', 'repository', 'comment', and 'exercises' state
+    addAssignment(
+      {
+        comment,
+        part: "React",
+        repository,
+        usedHours: hours,
+      },
+      uid
+    );
+    // Reset the form fields
+    setHours(0);
+    setRepository("");
+    setComment("");
+    displayAssignmentFormHandler();
+  };
+
+  return (
+    <form
+      style={{
+        position: "fixed",
+        zIndex: 1,
+        backgroundColor: "lightgreen",
+        top: "10%",
+      }}
+      onSubmit={handleSubmit}
+    >
+      <div>
+        <label htmlFor="hours">Used hours:</label>
+        <input
+          type="number"
+          id="hours"
+          value={hours}
+          onChange={handleHoursChange}
+          style={{ width: "100%", padding: "5px", marginBottom: "10px" }}
+        />
+      </div>
+      <div>
+        <label htmlFor="repository">GitHub repository:</label>
+        <input
+          type="text"
+          id="repository"
+          value={repository}
+          onChange={handleRepositoryChange}
+          style={{ width: "100%", padding: "5px", marginBottom: "10px" }}
+        />
+      </div>
+      <div>
+        <label htmlFor="comment">Comment:</label>
+        <textarea
+          id="comment"
+          value={comment}
+          onChange={handleCommentChange}
+          style={{ width: "100%", padding: "5px", marginBottom: "10px" }}
+        />
+      </div>
+
+      <button
+        type="submit"
+        style={{
+          backgroundColor: "#4CAF50",
+          color: "white",
+          padding: "10px",
+          border: "none",
+          cursor: "pointer",
+          marginTop: "10px",
+        }}
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
