9bef2c210bbd332bfc63d6610baad9baf4ead6a5
Ramazani Mwemedi <75116349+RamazaniMwemedi@users.noreply.github.com>
2023-06-24 13:02:51 +0300

    Added algolia API Keys

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -128,6 +128,35 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "X7F7HPFI1C",
+
+        // Public API key: it is safe to commit it
+        apiKey: "b7f66f7ef62879921d9bde3b2e516160",
+
+        indexName: "codust-tutorial",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: "/docs/", // or as RegExp: /\/docs\//
+          to: "/",
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
+      },
     }),
 };
 
