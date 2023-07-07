e54c45130bda20c57250b66de8e015665bf884c0
Ramazani Mwemedi <75116349+RamazaniMwemedi@users.noreply.github.com>
2023-07-07 11:06:16 +0300

    Added admin pages

---
+++ src/pages/assignment/admin.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+const Admin = () => {
+  return (
+    <Layout title={`Assignment Admin`} description="Assignment Admin">
+        <Box>
+            <Typography variant="h4">Admin Page</Typography>
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+        </Box>
+    </Layout>
+  );
+};
+
+export default Admin;
